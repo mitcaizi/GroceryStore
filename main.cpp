b5a0598edebdaf30e4e23623c2cb6d76cb6f2055
mitcaizi <jling1@mail.csuchico.edu>
2018-02-21 23:32:31 -0800

    secondpart

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  
 
 //while (numItems !=Q || numItems !=q)
 do 
@@ -17,8 +18,22 @@ do
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
- }
- while (!(input =='Q' || input =='q'));
+if (input == 'a' || input == 'A')
+{
+if (numItems!=5)
+{
+cout<<"What is the item?"<<endl;
+cin>>list[numItems];
+numItems++;
+}
+else
+{
+cout<<"You'll need a bigger list!"<<endl;
+}
+}
+}
+while (!(input =='Q' || input =='q') && numItems !=5);
+
 
   return 0;
 }
