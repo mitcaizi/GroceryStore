3c098a893380310f12c9160dc500e493f20399e2
mitcaizi <jling1@mail.csuchico.edu>
2018-03-13 22:32:24 -0700

    new

--- main.cpp
+++ main.cpp
@@ -1,44 +1,48 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string item;
-  
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string item;
+        vector<string> newitem;
 
-//while (numItems !=Q || numItems !=q)
-do 
-{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-if (input == 'a' || input == 'A')
-{
-cout<<"What is the item?"<<endl;
-cin>>item;
-if (numItems !=5)
-{
-list[numItems]=item;
-numItems++;
-}
-else
-{
-cout<<"You'll need a bigger list!"<<endl;
-}
-}
-}
-while (!(input =='Q' || input =='q'));
-cout<<"==ITEMS TO BUY=="<<endl;
-for (int x=0; x<5; x++)
-{
-cout<<x+1<<" "<<list[x]<<endl;
-}
-  return 0;
+
+        //while (numItems !=Q || numItems !=q)
+        do 
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if (input == 'a' || input == 'A')
+                {
+                        cout<<"What is the item?"<<endl;
+                        cin>>item;
+
+                        newitem.push_back(item);
+                        numItems++;
+                }
+
+
+        }
+        while (!(input =='Q' || input =='q'));
+        if (newitem.empty() == true)
+        {
+                cout<<"No items to buy!"<<endl;
+        }
+        else
+        {
+                for (int x=0; x<numItems; x++)
+                {
+                        cout<<newitem[x]<<endl;
+                }
+        }
+        return 0;
 }
